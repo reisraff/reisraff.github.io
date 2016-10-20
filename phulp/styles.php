ad14232f37350fb07d86ef66a2dbb5d6addb7b3a
Rafael Reis <reisraff@gmail.com>
2016-10-20 19:30:42 -0200

    removing some stuff

--- phulp/styles.php
+++ phulp/styles.php
@@ -1,15 +1,10 @@
 <?php
 
 $phulp->task('styles', function ($phulp) use ($config) {
-    // var sassOptions = {
-    // style: 'expanded'
-    // };
-
     $injectFiles = $phulp->src([$config['src'] . '/app'], '/.+(?<!app)\.scss$/');
 
     $phulp->src([$config['src'] . '/app'], '/app\.scss/', false)
         ->pipe(new Inject($injectFiles))
-        // ->pipe(wiredep(_.extend({}, conf.wiredep))) // inject bower dependencies
         ->pipe(new ScssCompiler(['importPaths' => ['src/src/app/']]))
         ->pipe($phulp->dest($config['tmp'] . '/serve/app/'));
 });
