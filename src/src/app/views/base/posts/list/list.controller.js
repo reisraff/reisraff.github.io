4a2858d3cb807ddfa7df6bad2b693d774207c822
Rafael Reis <reisraff@gmail.com>
2016-11-01 14:32:01 -0200

    Adding post

--- src/src/app/views/base/posts/list/list.controller.js
+++ src/src/app/views/base/posts/list/list.controller.js
@@ -4,10 +4,24 @@
   /** @ngInject */
   angular.module('app').controller(
     'PostsListController',
-    function PostsListController(PostsResolve) {
+    function PostsListController($stateParams, PostService, PostsResolve) {
       var _self = this;
 
       _self.posts = PostsResolve;
+      _self.currentPage = parseInt($stateParams.page);
+
+      _self.next = function () {
+        _self.currentPage--;
+        PostService.list(_self.currentPage).then(function (data) {
+          for (var i = 0; i < data.length; i++) {
+            _self.posts.push(data[i]);
+          }
+        });
+      };
+
+      if (_self.posts.length < 5) {
+        _self.next();
+      }
     }
   );
 
