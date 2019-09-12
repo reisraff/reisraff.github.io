baa7c502d64094ed68d35846813c6be886a04450
Rafael Reis <reisraff@gmail.com>
2019-09-12 20:07:34 -0300

    analytics

--- src/src/main.js
+++ src/src/main.js
@@ -4,11 +4,18 @@ import router from './router'
 
 import App from './App'
 
+import VueAnalytics from 'vue-analytics'
+
 import localStorage from './provider/localStorage'
 import lang from './provider/lang'
 
 Vue.use(localStorage)
 Vue.use(lang)
+
+Vue.use(VueAnalytics, {
+  id: 'UA-73718792-1',
+  router
+})
 
 /* eslint-disable no-new */
 new Vue({
