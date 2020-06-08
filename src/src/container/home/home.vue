dc5c98c7766c873ce68381c91938a0c29f15c863
Rafael Reis <reisraff@gmail.com>
2020-06-08 13:25:29 -0300

    telegram

--- src/src/container/home/home.vue
+++ src/src/container/home/home.vue
@@ -28,22 +28,25 @@
                   <a href="https://br.linkedin.com/in/reisraff" target="_blank"><img alt="linkedin" class="svg" src="../../assets/image/linkedin.svg"></a>
                 </div>
                 <div class="column has-text-centered">
-                  <a href="mailto:reisraff@gmail.com"><img alt="email" class="svg" src="../../assets/image/email.svg"></a>
+                  <a href="mailto:reisraff@gmail.com" target="_blank"><img alt="email" class="svg" src="../../assets/image/email.svg"></a>
                 </div>
                 <div class="column has-text-centered">
-                  <a href="tel:+5511984416276"><img alt="whatsapp" class="svg" src="../../assets/image/whatsapp.svg"></a>
+                  <a href="tel:+5511984416276" target="_blank"><img alt="whatsapp" class="svg" src="../../assets/image/whatsapp.svg"></a>
+                </div>
+                <div class="column has-text-centered">
+                  <a href="https://t.me/reisraff target="_blank""><img alt="telegram" style="width: 51px" class="svg" src="../../assets/image/telegram.png"></a>
                 </div>
                 <div class="column has-text-centered">
                   <a href="https://twitter.com/reisraff" target="_blank"><img alt="twitter" class="svg" src="../../assets/image/twitter.svg"></a>
                 </div>
                 <div class="column has-text-centered">
-                  <a href="https://discordapp.com/users/@reisraff#2191" target="_blank"><img style="height: 56.6934px" alt="twitter" class="svg" src="../../assets/image/discord.svg"></a>
+                  <a href="https://discordapp.com/users/@reisraff#2191" target="_blank"><img style="width: 51px" alt="twitter" class="svg" src="../../assets/image/discord.svg"></a>
                 </div>
                 <div class="column has-text-centered">
                   <a href="https://github.com/reisraff" target="_blank"><img alt="github" class="svg" src="../../assets/image/github.svg"></a>
                 </div>
                 <div class="column has-text-centered">
-                  <a href="https://www.reddit.com/user/reisraff" target="_blank"><img style="height: 56.6934px" alt="reddit" class="svg" src="../../assets/image/reddit.png"></a>
+                  <a href="https://www.reddit.com/user/reisraff" target="_blank"><img style="width: 51px" alt="reddit" class="svg" src="../../assets/image/reddit.png"></a>
                 </div>
             </div>
           </div>
