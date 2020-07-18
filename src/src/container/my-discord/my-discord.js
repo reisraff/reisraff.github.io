03dcd2b19cc0827f9bcad73784a57be6c5f7cac5
Rafael Reis <reisraff@gmail.com>
2020-07-18 10:20:46 -0300

    byte off

--- src/src/container/my-discord/my-discord.js
+++ src/src/container/my-discord/my-discord.js
@@ -6,21 +6,21 @@ const TRANSLATIONS = {
   'en': {
     'title': 'My Discord',
     'subtitle': 'This server there is the purpose of to share our knowledge with others, be welcome.',
-    'discordSubtitle': 'One bit to get it right',
+    'discordSubtitle': 'A single byte that make all difference',
     'online': 'Online',
     'join': 'Join',
   },
   'es': {
     'title': 'Mio Discord',
     'subtitle': 'Este servidor tiene el proposito de compartir nuestros conocimientos con otras personas, bien venido.',
-    'discordSubtitle': 'Uno bit para hacerlo bien',
+    'discordSubtitle': 'Un unico byte que hace la diferencia',
     'online': 'Online',
     'join': 'Entrar',
   },
   'pt': {
     'title': 'Meu Discord',
     'subtitle': 'Esse server tem como proposito compartilhar nosso conhecimento com outras pessoas, seja bem vindo.',
-    'discordSubtitle': 'Um bit para compreender',
+    'discordSubtitle': 'Um unico byte que faz toda a diferença',
     'online': 'Online',
     'join': 'Entrar',
   },
@@ -34,7 +34,7 @@ export default {
       trans: TRANSLATIONS,
       discord: {
         avatar: 'https://cdn.discordapp.com/icons/639468436027867176/1c60fad6430a4e1ca40500d22879a728.png?size=128',
-        title: 'Bit Off',
+        title: 'Byte Off',
         online: 0
       }
     }
